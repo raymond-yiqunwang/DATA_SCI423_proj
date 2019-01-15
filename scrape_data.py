bf81bbc8232533f05a6106db94c631e736c3762e
Raymond Wang <raymondwang@u.northwestern.edu>
2019-01-15 16:41:34 -0600

    update

--- scrape_data.py
+++ scrape_data.py
@@ -1,62 +1,84 @@
-## /home/raymond/.conda/envs/scrape/bin/python
-## Author: Raymond Wang <raymondwang@u.northwestern.edu>
-## Challenge project from Citrine Informatics
+#! /home/raymond/.conda/envs/scrape/bin/python
+# Author: Raymond Wang <raymondwang@u.northwestern.edu>
+# Challenge project from Citrine Informatics
+# Functionality: Scrape alloy steel data from MatWeb and save to csv files
 
+#from xvfbwrapper import Xvfb
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-from xvfbwrapper import Xvfb
 import pandas as pd
 import numpy as np
 import time
 import sys
+import os.path
+import os
 
 # setting virtual display, comment these lines for visualization
-display = Xvfb()
-display.start()
+#display = Xvfb()
+#display.start()
 
 # navigate to target website
-driver = webdriver.Chrome()
-driver.get("http://www.matweb.com/search/CompositionSearch.aspx")
+options = webdriver.ChromeOptions()
+#options.add_argument("headless") # uncomment this for visualization
+driver_path = os.getcwd() + "/chromedriver-linux"
+#driver_path = os.getcwd() + "/chromedriver-mac"
+driver = webdriver.Chrome(executable_path=driver_path, chrome_options=options)
+driver.get("http://www.matweb.com/index.aspx")
+
+# user login, less likely to be blocked
+driver.find_element_by_link_text("LOG IN").click()
+time.sleep(2)
+username = driver.find_element_by_id("ctl00_ContentMain_txtUserID")
+username.send_keys("raymond931118@gmail.com")
+passwd =  driver.find_element_by_id("ctl00_ContentMain_txtPassword")
+passwd.send_keys("abcd1234")
+time.sleep(0.5)
+driver.find_element_by_xpath("//input[@src='/images/buttons/btnLogin.gif']").click()
+time.sleep(3)
+
+# Alloy composition
+driver.find_element_by_link_text("Alloy Composition").click()
 
 # unfold "Ferrous Metal"
 driver.find_element_by_id("ctl00_ContentMain_ucMatGroupTree_LODCS1_msTreeViewn1").click()
-time.sleep(1) # wait for the browser
+time.sleep(2) # wait for the browser
 
 # choose "Alloy Steel"
 driver.find_element_by_id("ctl00_ContentMain_ucMatGroupTree_LODCS1_msTreeViewt6").click()
-time.sleep(1)
+time.sleep(2)
 
 # choose composition1: Mn
 select_fe = Select(driver.find_element_by_id("ctl00_ContentMain_ucPropertyDropdown1_drpPropertyList"))
 select_fe.select_by_visible_text("Manganese, Mn")
 driver.find_element_by_id("ctl00_ContentMain_ucPropertyEdit1_txtpMin").send_keys("0.0")
-time.sleep(1)
+time.sleep(2)
 
 # choose composition2: Cr
 select_cr = Select(driver.find_element_by_id("ctl00_ContentMain_ucPropertyDropdown2_drpPropertyList"))
 select_cr.select_by_visible_text("Chromium, Cr")
 driver.find_element_by_id("ctl00_ContentMain_ucPropertyEdit2_txtpMin").send_keys("0.0")
-time.sleep(1)
+time.sleep(2)
 
 # choose composition3: Ni
 select_ni = Select(driver.find_element_by_id("ctl00_ContentMain_ucPropertyDropdown3_drpPropertyList"))
 select_ni.select_by_visible_text("Nickel, Ni")
 driver.find_element_by_id("ctl00_ContentMain_ucPropertyEdit3_txtpMin").send_keys("0.0")
-time.sleep(1)
+time.sleep(2)
 
 # lauching search
 driver.find_element_by_id("ctl00_ContentMain_btnSubmit").click()
-time.sleep(2)
+time.sleep(3)
 
 # show 200 per page
 instance_per_page = 200
 Select(driver.find_element_by_id("ctl00_ContentMain_UcSearchResults1_drpPageSize1")).select_by_visible_text(str(instance_per_page))
-time.sleep(2)
+time.sleep(3)
 
 file_names = []
 
 # loop over 5 pages of results to collect data
-for ipage in range(5):
+npages = 5
+for ipage in range(npages):
     print("currently on page " + str(ipage+1))
     sys.stdout.flush()
 
@@ -70,7 +92,14 @@ for ipage in range(5):
         fname = name.replace(" ", "_")
         fname = fname.replace("/", "_")
         fname = fname.replace(",", "")
-        if len(fname) > 200: fname = fname[:200]+"..." # in case length of file name exceeds Linux limit (255B)
+        if len(fname) > 240: fname = fname[:240]+"..." # in case length of file name exceeds Linux limit (255B)
+        pathname = "data/" + fname + ".csv"
+        if os.path.isfile(pathname):
+            if os.stat(pathname).st_size == 0: # in case last written unsuccessful (e.g. blocked)
+                print("  removing: " + pathname + ".csv")
+                os.remove(pathname)
+            else:
+                continue
         print('  processing: \"' + fname + '\"')
         sys.stdout.flush()
         
@@ -80,15 +109,15 @@ for ipage in range(5):
         
         # navigate into each alloy page
         driver.find_element_by_link_text(name).click()
-        time.sleep(2)
+        time.sleep(5)
         table = driver.find_element_by_xpath("//table[@class='tabledataformat']")
         attrib = []
         for row in table.find_elements_by_xpath("//tr[@class='altrow datarowSeparator']"):
             attrib.append([d.text for d in row.find_elements_by_css_selector('td')])
-            time.sleep(0.1)
+            time.sleep(0.5)
         for row in table.find_elements_by_xpath("//tr[@class=' datarowSeparator']"):
             attrib.append([d.text for d in row.find_elements_by_css_selector('td')])
-            time.sleep(0.1)
+            time.sleep(0.5)
         attrib = np.array(attrib)
         
         # write to file
@@ -96,15 +125,17 @@ for ipage in range(5):
         df.to_csv(f)
         f.close()
         driver.back()
-        time.sleep(2)
+        time.sleep(5)
 
-    # navidate to the next page
+    # navigate to the next page
     driver.find_element_by_id("ctl00_ContentMain_UcSearchResults1_lnkNextPage").click()
-    time.sleep(3)
+    time.sleep(5)
 
 # store file names
 dfname = pd.DataFrame(file_names, dtype=str)
 dfname.to_csv("data/filenames.csv", header=False, index=False)
+
+# quit chrome driver
 driver.quit()
 
 # virtual display off
