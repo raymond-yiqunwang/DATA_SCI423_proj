5d93960c1fbb67908b7f748b440d0b2eb2c1a9f4
Raymond Wang <raymondwang@u.northwestern.edu>
2019-01-15 14:01:16 -0600

    debug

--- scrape_data.py
+++ scrape_data.py
@@ -8,6 +8,7 @@ from xvfbwrapper import Xvfb
 import pandas as pd
 import numpy as np
 import time
+import sys
 
 # setting virtual display, comment these lines for visualization
 display = Xvfb()
@@ -56,21 +57,27 @@ file_names = []
 
 # loop over 5 pages of results to collect data
 for ipage in range(5):
+    print("currently on page " + str(ipage+1))
+    sys.stdout.flush()
 
     # save name of datasets to a list before processing, may not be ideal design but avoids stale element problems
     name_list = []
     for item in driver.find_elements_by_xpath("//td[@style='width:auto; font-weight:bold;']"):
         name_list.append(item.text)
-    print("size of name list: " + str(len(name_list)))
-    file_names += name_list
     
     for name in name_list:
         print('currently processing: \"' + name + '\"')
-        f = open("data/" + name + ".csv", 'w')
+        sys.stdout.flush()
+        
+        # open file
+        fname = name
+        if len(fname) > 240: fname = fname[:240] # in case length of file name exceeds Linux limit
+        f = open("data/" + fname + ".csv", 'w')
+        file_names.append(fname)
+        
         # navigate into each alloy page
         driver.find_element_by_link_text(name).click()
         time.sleep(2)
-    
         table = driver.find_element_by_xpath("//table[@class='tabledataformat']")
         attrib = []
         for row in table.find_elements_by_xpath("//tr[@class='altrow datarowSeparator']"):
@@ -80,15 +87,19 @@ for ipage in range(5):
             attrib.append([d.text for d in row.find_elements_by_css_selector('td')])
             time.sleep(0.1)
         attrib = np.array(attrib)
+        
+        # write to file
         df = pd.DataFrame(data=attrib[:, 1:], columns=['Metric', 'English', 'Comments'], index=attrib[:, 0] )
         df.to_csv(f)
         f.close()
         driver.back()
         time.sleep(2)
 
+    # navidate to the next page
     driver.find_element_by_id("ctl00_ContentMain_UcSearchResults1_lnkNextPage").click()
     time.sleep(3)
 
+# store file names
 dfname = pd.DataFrame(file_names, dtype=str)
 dfname.to_csv("data/filenames.csv", header=False, index=False)
 driver.quit()
