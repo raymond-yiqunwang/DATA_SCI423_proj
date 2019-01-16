a9e6f1d42e39ec92f7f287377a2ae746024b944b
Raymond Wang <raymondwang@u.northwestern.edu>
2019-01-16 10:10:39 -0600

    minor

--- scrape_data.py
+++ scrape_data.py
@@ -3,7 +3,6 @@
 # Challenge project from Citrine Informatics
 # Functionality: Scrape alloy steel data from MatWeb and save to csv files
 
-#from xvfbwrapper import Xvfb
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import pandas as pd
@@ -13,35 +12,31 @@ import sys
 import os.path
 import os
 
-# setting virtual display, comment these lines for visualization
-#display = Xvfb()
-#display.start()
-
 # navigate to target website
 options = webdriver.ChromeOptions()
-#options.add_argument("headless") # uncomment this for visualization
-driver_path = os.getcwd() + "/chromedriver-linux"
-#driver_path = os.getcwd() + "/chromedriver-mac"
+options.add_argument("headless") # comment this for visualization
+#driver_path = os.getcwd() + "/chromedriver-linux"
+driver_path = os.getcwd() + "/chromedriver-mac"
 driver = webdriver.Chrome(executable_path=driver_path, chrome_options=options)
 driver.get("http://www.matweb.com/index.aspx")
 
 # user login, less likely to be blocked
-driver.find_element_by_link_text("LOG IN").click()
-time.sleep(2)
-username = driver.find_element_by_id("ctl00_ContentMain_txtUserID")
-username.send_keys("raymond931118@gmail.com")
-passwd =  driver.find_element_by_id("ctl00_ContentMain_txtPassword")
-passwd.send_keys("abcd1234")
-time.sleep(0.5)
-driver.find_element_by_xpath("//input[@src='/images/buttons/btnLogin.gif']").click()
-time.sleep(3)
+#driver.find_element_by_link_text("LOG IN").click()
+#time.sleep(2)
+#username = driver.find_element_by_id("ctl00_ContentMain_txtUserID")
+#username.send_keys("raymond931118@gmail.com")
+#passwd =  driver.find_element_by_id("ctl00_ContentMain_txtPassword")
+#passwd.send_keys("abcd1234")
+#time.sleep(0.5)
+#driver.find_element_by_xpath("//input[@src='/images/buttons/btnLogin.gif']").click()
+#time.sleep(3)
 
 # Alloy composition
 driver.find_element_by_link_text("Alloy Composition").click()
 
 # unfold "Ferrous Metal"
 driver.find_element_by_id("ctl00_ContentMain_ucMatGroupTree_LODCS1_msTreeViewn1").click()
-time.sleep(2) # wait for the browser
+time.sleep(2)
 
 # choose "Alloy Steel"
 driver.find_element_by_id("ctl00_ContentMain_ucMatGroupTree_LODCS1_msTreeViewt6").click()
@@ -65,7 +60,7 @@ select_ni.select_by_visible_text("Nickel, Ni")
 driver.find_element_by_id("ctl00_ContentMain_ucPropertyEdit3_txtpMin").send_keys("0.0")
 time.sleep(2)
 
-# lauching search
+# lauch searching process
 driver.find_element_by_id("ctl00_ContentMain_btnSubmit").click()
 time.sleep(3)
 
@@ -73,8 +68,6 @@ time.sleep(3)
 instance_per_page = 200
 Select(driver.find_element_by_id("ctl00_ContentMain_UcSearchResults1_drpPageSize1")).select_by_visible_text(str(instance_per_page))
 time.sleep(3)
-
-file_names = []
 
 # loop over 5 pages of results to collect data
 npages = 5
@@ -93,19 +86,19 @@ for ipage in range(npages):
         fname = fname.replace("/", "_")
         fname = fname.replace(",", "")
         if len(fname) > 240: fname = fname[:240]+"..." # in case length of file name exceeds Linux limit (255B)
-        pathname = "data/" + fname + ".csv"
+        pathname = "data_raw/" + fname + ".csv"
         if os.path.isfile(pathname):
             if os.stat(pathname).st_size == 0: # in case last written unsuccessful (e.g. blocked)
                 print("  removing: " + pathname + ".csv")
                 os.remove(pathname)
             else:
+                print("  skipping: " + pathname + ".csv")
                 continue
         print('  processing: \"' + fname + '\"')
         sys.stdout.flush()
         
         # open file
-        f = open("data/" + fname + ".csv", 'w')
-        file_names.append(fname)
+        f = open("data_raw/" + fname + ".csv", 'w')
         
         # navigate into each alloy page
         driver.find_element_by_link_text(name).click()
@@ -131,12 +124,5 @@ for ipage in range(npages):
     driver.find_element_by_id("ctl00_ContentMain_UcSearchResults1_lnkNextPage").click()
     time.sleep(5)
 
-# store file names
-dfname = pd.DataFrame(file_names, dtype=str)
-dfname.to_csv("data/filenames.csv", header=False, index=False)
-
 # quit chrome driver
 driver.quit()
-
-# virtual display off
-display.stop()
