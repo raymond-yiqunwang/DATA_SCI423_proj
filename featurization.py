bc953df39e0d5fa9d0eac2be9002ea62910096f7
Raymond Wang <raymondwang@u.northwestern.edu>
2019-01-16 15:15:26 -0600

    minor

--- featurization.py
+++ featurization.py
@@ -1,7 +1,15 @@
+from collections import defaultdict
 import pandas as pd
 import os
 
+features = defaultdict(int)
+
 ddir = "./scrape_data/data_raw/"
 for filename in os.listdir(ddir):
-    data = pd.read_csv(ddir+filename)
-    print(data.shape)
+    data = pd.read_csv(ddir+filename, header=0, index_col=0)
+    for ind in data.index:
+        features[ind] += 1
+
+
+for ii in sorted(features.items(), key=lambda tup: tup[-1], reverse=True):
+    print(ii)
