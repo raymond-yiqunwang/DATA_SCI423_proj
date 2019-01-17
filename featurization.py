8d7956a3fd41513712b904d66952a02804561ed0
Raymond Wang <raymondwang@u.northwestern.edu>
2019-01-17 07:52:39 -0600

    feature

--- featurization.py
+++ featurization.py
@@ -2,14 +2,15 @@ from collections import defaultdict
 import pandas as pd
 import os
 
-features = defaultdict(int)
+features_all = defaultdict(int)
 
 ddir = "./scrape_data/data_raw/"
 for filename in os.listdir(ddir):
     data = pd.read_csv(ddir+filename, header=0, index_col=0)
     for ind in data.index:
-        features[ind] += 1
+        features_all[ind] += 1
 
-
-for ii in sorted(features.items(), key=lambda tup: tup[-1], reverse=True):
-    print(ii)
+print(' These are the potential features:')
+for ii in sorted(features_all.items(), key=lambda tup: tup[-1], reverse=True):
+    if (ii[-1] > 100) and (ii[0] is not ' '):
+        print(ii)
